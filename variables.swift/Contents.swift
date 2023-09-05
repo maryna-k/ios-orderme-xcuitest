325b73e577a61c2e4527aa2fc614e1fca875ce11
JuliaK-118 <ykaranevskaya@engenious.io>
2023-09-05 21:53:39 +0300

    variables

---
+++ variables.swift/Contents.swift
@@ -0,0 +1,11 @@
+//Link to the Swift documentation: https://docs.swift.org/swift-book/LanguageGuide/TheBasics.html
+import UIKit
+
+var str = "Hello, playground"
+
+var age: Int = 25
+
+let name: String = "Boris"
+
+age = 30
+name = "Igor"
